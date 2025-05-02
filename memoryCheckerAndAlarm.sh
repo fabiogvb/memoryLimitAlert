ab1e4a1aa0ea330477e6b60e0457aafb37555013
fabiogvb <34027544+fabiogvb@users.noreply.github.com>
2025-05-02 05:01:31 -0300

    Add files via upload

---
+++ memoryCheckerAndAlarm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+thresholdRamPercent=80 # Should be an integer
+
+warningMessage="WARNING: RAM usage above $thresholdRamPercent percentage.\n\
+Please free some RAM."
+
+while true ; do
+
+    # Get used RAM % via free(1) .
+    #usedRamPercent=$(free | grep Mem | awk '{print $3/$2 * 100.0}' | sed 's/\..*$//g')
+
+    # Get used RAM % via python's psutil.
+    usedRamPercent=$(python3 -c 'import psutil ; print(psutil.virtual_memory().percent)' | sed 's/\..*$//g')
+
+    if [ $usedRamPercent -ge $thresholdRamPercent ] ; then
+        # Send a notification
+        notify-send -u critical -i dialog-warning "$warningMessage"
+        # Speak it out.
+        spd-say "$warningMessage"
+        # Display the warning in a window.
+        # Waits until "OK" is clicked / window closed.
+        zenity --info --height=240 --width=480 --text "${warningMessage}\n\
+        Press OK to continue monitoring. Close this dialog to quit monitoring." || exit 1
+    fi
+
+    # Refresh every 5 seconds
+    sleep 5
+
+done
